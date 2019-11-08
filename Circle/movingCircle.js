0ed2122f3e48e9bcf5f9a45937177705b0c34730
Spacecase94 <45679995+Spacecase94@users.noreply.github.com>
2019-11-07 22:51:40 -0500

    Add files via upload
    Learning how to use the p5 library and learning JavaScript in a visual way

---
+++ Circle/movingCircle.js
@@ -0,0 +1,14 @@
+const circleX = 50;
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  // background
+  background(250, 250, 100);
+
+  // The circle
+  fill(250, 200, 200);
+  ellipse(circleX, 200, 80, 80);
+}
